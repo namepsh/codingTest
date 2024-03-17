9c02b8f74fcff3fd512e90dfa148988663ac1e5b
namepsh <141614578+namepsh@users.noreply.github.com>
2024-03-17 22:17:47 +0900

    Update 특수문자 출력하기.java

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        // \, ', " 을 출력할 때는 백슬래시(\)를 앞에 붙여 줘야 한다.
+        // 자바의 출력문에서 백슬래시가 등장하는 순간 바로 다음 문자를 인식해 상황에 맞게 처리함
+        // 이를 이스케이프 시퀀스(Escape Sequence)라고 한다.
         System.out.println("!@#$%^&*(\\'\"<>?:;");
     }
 }
